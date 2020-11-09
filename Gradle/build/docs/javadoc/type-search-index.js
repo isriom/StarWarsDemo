8901f04eb3aa82d7c009f1ff2fe0ad8e32773865
isriom <62965230+isriom@users.noreply.github.com>
2020-11-09 12:31:13 -0600

    gradle module

---
+++ Gradle/build/docs/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"demo.starwars.dto","l":"ApiResponse"},{"p":"demo.starwars","l":"Main"},{"p":"demo.starwars.dto","l":"People"}]
